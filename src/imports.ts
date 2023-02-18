98934e5239ee3ac24be123973b6a160657ba452c
{苏盼盼} <1372978934@qq.comqq.com>
2023-02-18 18:41:50 +0800

    complete-parse-imports

--- src/imports.ts
+++ src/imports.ts
@@ -33,22 +33,25 @@ function getAbsolutePos(this:Irespose,key:keyof Pick<Irespose,'from'|'import'> )
 
 export default (code:string)=>{
   code = stripComments(code)
-  const reg = /import([\s])+(\{[^}]*?\}|.*?)\1from\1+('|")(.*)?\3;?/g
+  const reg = /import([\s])+((\{([^}]*)?\}|.*?)\1from\1+)?('|")(.*)?\5;?/g
   let m = reg.exec(code)
   const res:Irespose[] = []
   while(m){
+    const imp = m[4] ? m[4] : m[3]
     const o:Irespose = {
       text:m[0],
       start:m.index,
       end:reg.lastIndex,
-      import:m[2],
-      from:m[4],
+      import:imp,
+      from:m[6],
       extra:{
         import:null,
         from:null
       }
     }
-    o.extra.import = getAbsolutePos.call(o,'import')
+    if(imp){
+      o.extra.import = getAbsolutePos.call(o,'import')
+    }
     o.extra.from = getAbsolutePos.call(o,'from')
     res.push(o)
     m = reg.exec(code)
